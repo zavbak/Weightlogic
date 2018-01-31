20192275ae91e26d1c29b8127b94e92181058117
zavbak <799000@mail.ru>
2018-02-01 00:00:07 +1000

    Добавил комментарий 6

--- app/src/main/java/ru/a799000/android/weightlogic/mvp/presenters/DetailBarcodePr.java
+++ app/src/main/java/ru/a799000/android/weightlogic/mvp/presenters/DetailBarcodePr.java
@@ -36,7 +36,7 @@ public class DetailBarcodePr extends MvpPresenter<DetailBarcodeView> implements 
     String mParamIdBarcode;
     String mParamBarcode;
 
-    //Комментарий****6
+    //Комментарий****7
 
 
     Product mProduct;
